96d65257efb03d389607da0a581dfa75bd159be9
FroStyRo <62169048+VostrikovRoman@users.noreply.github.com>
2025-04-05 11:09:25 +0500

    2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from 
 
 app = FastAPI()
 
@@ -6,3 +7,15 @@ app = FastAPI()
 def get_items():
     return 0
 
+@app.post("/t_square")
+def t_square(a: Annotated[int, Query(ge=0)], b: Annotated[int, Query(ge=0)], c:Annotated[int, Query(ge=0)]):
+    p = a+b+c
+    if (a+b>c) and (b+c>a) and (a+c>b):
+        h_p = p/2
+        s = (h_p*(h_p-a)*(h_p-b)*(h_p-c))**(1/2)
+        return {
+            'perimetr' : p,
+            'square': s
+        }
+    else:
+        return 'Треугольника с указанными сторонами не существует'
