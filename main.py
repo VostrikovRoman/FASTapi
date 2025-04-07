524a5200d142653b26d344f271547f254db6f3e2
FroStyRo <62169048+VostrikovRoman@users.noreply.github.com>
2025-04-07 15:03:30 +0500

    Готово

--- main.py
+++ main.py
@@ -4,24 +4,122 @@ from typing import Annotated
 app = FastAPI()
 
 items = [
-    {'name': 'Шоколад Алёнка', 'price': '100', 'description':'Молочный шоколад Алёнка'},
-    {'name':'Snickers', 'price':'150', 'description':'Шоколадный батончик Snickers с арахисом'},
-    {'name':'Молоко Молочная благодать', 'price':'84', 'description':'Молоко свежее пастеризованное Молочная благодать 0,9л'},
-    {'name':'Pulpy апельсин', 'price':'109', 'description':'Напиток сокосодержащий из апельсина с мякотью'},
-    {'name':'Flash оригинальный 1л', 'price':'132', 'description':'Напиток безалкогольный сильногазированный Флэш ап макс'},
-    {'name':'Grand Desert белый шоколад с клубничным муссом', 'price':'154', 'description':'Пудинг молочный ультрапастеризованный с белым шоколадом и сливочно-клубничным муссом'},
+  {
+    "name": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
+  },
+  {
+    "name": "Mens Casual Premium Slim Fit T-Shirts ",
+    "price": 22.3,
+    "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket."
+  },
+  {
+    "name": "Mens Cotton Jacket",
+    "price": 55.99,
+    "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day."
+  },
+  {
+    "name": "Mens Casual Slim Fit",
+    "price": 15.99,
+    "description": "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description."
+  },
+  {
+    "name": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+    "price": 695,
+    "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection."
+  },
+  {
+    "name": "Solid Gold Petite Micropave ",
+    "price": 168,
+    "description": "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days."
+  },
+  {
+    "name": "White Gold Plated Princess",
+    "price": 9.99,
+    "description": "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day..."
+  },
+  {
+    "name": "Pierced Owl Rose Gold Plated Stainless Steel Double",
+    "price": 10.99,
+    "description": "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel"
+  },
+  {
+    "name": "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+    "price": 64,
+    "description": "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system"
+  },
+  {
+    "name": "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+    "price": 109,
+    "description": "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)"
+  },
+  {
+    "name": "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5",
+    "price": 109,
+    "description": "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability."
+  },
+  {
+    "name": "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
+    "price": 114,
+    "description": "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty"
+  },
+  {
+    "name": "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
+    "price": 599,
+    "description": "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz"
+  },
+  {
+    "name": "Samsung 49-Inch CHG90 144Hz Curved Gaming Monitor (LC49HG90DMNXZA) – Super Ultrawide Screen QLED ",
+    "price": 999.99,
+    "description": "49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag"
+  },
+  {
+    "name": "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
+    "price": 56.99,
+    "description": "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates"
+  }
 ]
 
 @app.get("/items")
-def get_items(name: Annotated[str|None, Query(min_length=2)] = None, min_price: Annotated[int|None, Query(gt = 0)] = None, max_price:Annotated[int|None, Query(gt=0)]=None):
+def get_items(name: str|None = Query(min_length=2, default=None), min_price: int|None = Query(gt = 0, default=None), max_price: int|None = Query(gt=0, default=None), limit: int = Query(gt=0, default=10, lt=100)):
+    res_name_items=[]
     result_items = []
+
+    if name != None:
+        for i in items:
+            if name in i.get('name'):
+                res_name_items.append(i)
+    else:
+        res_name_items = items
+
+    if min_price != None and max_price != None:
+        if min_price>=max_price:
+            return 'Ошибка!'
+        else:
+            for i in res_name_items:
+                if min_price <= i.get('price') <= max_price:
+                    result_items.append(i)   
+    else:
+        if min_price != None:
+            for i in res_name_items:
+                if min_price <= i.get('price'):
+                    result_items.append(i)
+        elif max_price != None:
+            for i in res_name_items:
+                if max_price >= i.get('price'):
+                    result_items.append(i)
+        else:
+            result_items = res_name_items
+
+    if limit != None:
+        return result_items[:limit]
+    else:
+        return result_items
+            
     
-    if min_price>max_price:
-        return 'Ошибка!'
-    for i in items:
-        if name in i.get('name'):
-            result_items.append(i)
-    return result_items
+    
+    
 
 # @app.post("/t_square")
 # def t_square(a: Annotated[int, Query(ge=0)], b: Annotated[int, Query(ge=0)], c:Annotated[int, Query(ge=0)]):
